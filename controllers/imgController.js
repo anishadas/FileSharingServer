f30d4344833e52375c7df46849ef47024a74842e
Anisha <payalad10@gmail.com>
2023-07-24 15:30:18 +0530

    base url

--- controllers/imgController.js
+++ controllers/imgController.js
@@ -1,14 +1,17 @@
 import FileModel from "../models/model.js";
+import dotenv from 'dotenv';
+dotenv.config();
+const BASE_URL = process.env.BASE_URL;
 
 // uploading the file to db
 export const imgUploader = async (req, res) => {
-    console.log(req.file);
+    // console.log(req.file);
     const { path, originalname } = req.file;
     const newFile = new FileModel({ path, name:originalname });
     try {
         await newFile.save();
         // console.log( `http://localhost:8000/file/${newFile._id}`)
-        return res.status(200).json({ path: `http://localhost:8000/file/${newFile._id}` });
+        return res.status(200).json({ path: `${BASE_URL}/file/${newFile._id}` });
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
@@ -24,6 +27,7 @@ export const imgDownloader = async (req, res) => {
         file.downloadContent++;
         await file.save();
         // we dont have to send the file, we have to download it
+        console.log(file.path)
         res.download(file.path, file.name);
     } catch (err) {
         console.error(err.message);
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 app.use('/', router);
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch(err => console.log(err.message))
